e8d455ff30bd7d73b2a1db893add4c2fe998199c
Noob <ExplodingFKL@gmail.com>
2019-09-18 11:13:47 +0800

    fixed bugs.

--- udpSocket/src/main/java/top/fksoft/server/udp/UdpServer.kt
+++ udpSocket/src/main/java/top/fksoft/server/udp/UdpServer.kt
@@ -1,5 +1,7 @@
 package top.fksoft.server.udp
 
+import top.fksoft.server.udp.bean.Packet
+import top.fksoft.server.udp.utils.obser.Observable
 import java.io.Closeable
 import java.net.DatagramSocket
 import java.net.SocketException
@@ -8,8 +10,8 @@ import java.net.SocketException
  * # 建立一个 UDP 管理服务器
  * > 封装 的DatagramSocket
  */
-class UdpServer(private val datagramSocket: DatagramSocket = DatagramSocket()):Closeable{
-
+class UdpServer(private val datagramSocket: DatagramSocket = DatagramSocket()) : Closeable {
+    private val observable = Observable<Packet>()
     init {
         if (datagramSocket.isBound.not() || datagramSocket.isClosed) {
             throw SocketException("连接存在问题，无法初始化.")
@@ -17,12 +19,10 @@ class UdpServer(private val datagramSocket: DatagramSocket = DatagramSocket()):C
     }
 
 
-
-
     /**
      * 得到本地端口号
      */
-    private val localPort by lazy{
+    private val localPort by lazy {
         datagramSocket.localPort
     }
 
@@ -30,4 +30,11 @@ class UdpServer(private val datagramSocket: DatagramSocket = DatagramSocket()):C
 
     }
 
+
+    companion object{
+        /**
+         * 标准 Internet 下 MTU 大小
+         */
+        const val InternetMTU = 576
+    }
 }
---
+++ udpSocket/src/main/java/top/fksoft/server/udp/bean/Packet.kt
@@ -0,0 +1,9 @@
+package top.fksoft.server.udp.bean
+
+/**
+ *  # 数据包实体类
+ *   针对UDP数据包
+ */
+ open abstract class Packet {
+
+}
